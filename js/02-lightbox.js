f8e367a512271f98a2bd67acd4cdb09a4080d54d
varunyk1805 <varunyk1805@gmail.com>
2021-11-12 05:49:46 +0200

    add completed 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const images = galleryItems.map(image => 
+    `<a class="gallery__item" href="${image.original}">
+        <img
+            class="gallery__image"
+            src="${image.preview}"
+            alt="${image.description}"
+        />
+    </a>`
+).join('');
+
+gallery.insertAdjacentHTML('beforeend', images);
+
+
+gallery.addEventListener('click', event => {
+    event.preventDefault();
+    new SimpleLightbox('.gallery__item',
+        {
+            captionsData: 'alt',
+            captionPosition: 'bottom',
+            captionDelay: 250
+        });
+})
